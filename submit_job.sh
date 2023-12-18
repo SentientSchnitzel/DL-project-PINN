aa19d7806aa712321beb0d536309a2c87cd36cdb
Theodor <theodorgto@gmailcom>
2023-12-18 10:16:12 +0100

    .sh ignor

--- submit_job.sh
+++ submit_job.sh
@@ -10,17 +10,17 @@
 #BSUB -R "rusage[mem=2GB]"
 #BSUB -gpu "num=1"
 #BSUB -M 3GB
-### wall-time 8 hr
+### wall-time 2 hr
 #BSUB -W 8:00 
-#BSUB -u simondanielschneider@gmail.com # change this to your own :)
+#BSUB -u s194055@student.dtu.dk # change this to your own :)
 #BSUB -N
 
 ### Doesnt overwrite, just makes nuew. 
 #BSUB -o j_outputs/Output_%J.out 
 #BSUB -e j_errors/Error_%J.err
 
-source .venv/bin/activate
-module load cuda/12.0
+### run shell script to activate virtual environment
+source act_venv.sh
 
 ### Write which script to run below
-python3 wave_eq_pinn_minibatching.py --exp_id #$exp_id
+python3 wave_eq_ffn.py
